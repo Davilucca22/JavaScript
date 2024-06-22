15c6fadfa400a9420a6e891e1fbe6c7a1f19a1d8
davilucas <davilucarios47@gmail.com>
2024-06-22 20:20:30 -0300

    finish

--- exercicios/ex002/ex002.js
+++ exercicios/ex002/ex002.js
@@ -1,29 +1,9 @@
-   
 
-function liga(){
-    //pega os valores das caixas de marcaçao
-    const homem = document.querySelector('#masc')
-    const mulher = document.querySelector('#fem')
-    if(homem.value == "on" && mulher.value == "off"){
-        homem.value = "off"
-        mulher.value = "on"
-    }else{
-        homem.value = "on"
-        mulher.value = "off"
-    }
-}
 
 function verificar(){
-    const homem = document.querySelector('#masc')
-    const mulher = document.querySelector('#fem')
-    let texto = document.querySelector('#texto')
-
     //pega o ano atual
     var data = new Date()
     var ano = data.getFullYear()
-
-    //pega a imagem 
-    var img = document.querySelector('#imagem')
 
     //pega o valor do ano digitado pelo usuario
     var nasc = document.querySelector('#anonasc')
@@ -32,41 +12,41 @@ function verificar(){
     //calcula a idade do usuario
     var idade = ano - nascAno
 
-    if(idade <= 12 && homem.value == "on"){
-        texto.innerHTML = `<p>Ola Jovem Rapaz,voce tem ${idade} anos</p>`
-        img.src ="imagens/homemcriança.jpg";
-        img.alt ="criança";
-    }else if(idade <= 12 && mulher.value == "on"){
-        texto.innerHTML = `<p>Ola Jovem Moça,voce tem ${idade} anos</p>`
-        img.src = "imagens/mulhercriança.jpg"
-        img.alt = "criança"
-    }else if(idade <= 22 && homem.value == "on"){
-        texto.innerHTML = `<p>Ola Rapaz,voce tem ${idade} anos</p>`
-        img.src = "imagens/homemadolescente.jpg"
-        img.alt = "adolescente"
-    }else if(idade <= 22 && mulher.value == "on"){
-        texto.innerHTML = `<p>Ola Moça,voce tem ${idade} anos</p>`
-        img.src = "imagens/mulheradolescente.jpg"
-        img.alt = "adolescente"
-    }else if(idade <= 50 && homem.value == "on"){
-        texto.innerHTML = `<p>Ola Senhor,voce tem ${idade} anos</p>`
-        img.src = "imagens/homemadulto.jpg"
-        img.alt = "adulto"
-    }else if(idade <= 50 && mulher.value == "on"){
-        texto.innerHTML = `<p>Ola Senhora,voce tem ${idade} anos</p>`
-        img.src = "imagens/mulheradulta.jpg"
-        img.alt = "adulta"
-    }else if(idade > 50 && homem.value == "on"){
-        texto.innerHTML = `<p>Ola Senhor,voce tem ${idade} anos</p>`
-        img.src = "imagens/homemidoso.jpg"
-        img.alt = "idoso"
-    }else if(idade > 50 && mulher.value == "on"){
-        texto.innerHTML = `<p>Ola Senhora,voce tem ${idade} anos</p>`
-        img.src = "imagens/mulheridosa.jpg"
-        img.alt = "idosa"
+    if(Number(anonasc.value) >= ano || Number(anonasc.value) <= 0){
+        alert('valores invalidos')
+
     }else{
-        alert('[ERROR] valores invalidos')
+        var result = document.querySelector('#saida')
+        var genero = ''
+        var sexo = document.getElementsByName('sex')
+        var img = document.createElement('img')
+        if(sexo[0].checked){
+            genero = 'Homem'
+            if(idade >= 0 && idade < 10){
+                img.setAttribute('src','imagens/homemcriança.jpg')
+            }else if(idade < 20){
+                img.setAttribute('src','imagens/homemadolescente.jpg')
+            }else if(idade < 50){
+                img.setAttribute('src', 'imagens/homemadulto.jpg')
+            }else {
+                img.setAttribute('src','imagens/homemidoso.jpg')
+            }
+
+        }else if (sexo[1].checked){
+            genero = 'Mulher'
+            if(idade >= 0 && idade < 10){
+                img.setAttribute('src','imagens/mulhercriança.jpg')
+            }else if(idade < 20){
+                img.setAttribute('src','imagens/mulheradolescente.jpg')
+            }else if(idade < 50){
+                img.setAttribute('src','imagens/mulheradulta.jpg')
+            }else{
+                img.setAttribute('src','imagens/mulheridosa.jpg')
+            }
+        }
     }
+    result.innerHTML = `Detectamos ${genero} com ${idade} anos`
+    result.appendChild(img)
 
 }
 
