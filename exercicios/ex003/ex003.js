2e5d9332c772f6f88d17c4fd8734128370941dab
davilucas <davilucarios47@gmail.com>
2024-06-29 19:59:33 -0300

    exercicio finalizado

--- exercicios/ex003/ex003.js
+++ exercicios/ex003/ex003.js
@@ -1,17 +1,33 @@
 function contar(){
 
     ///pega o valor das variaveis
-    var inicio = Number(document.querySelector('#inicio').value)
-    var fim = Number(document.querySelector('#fim').value)
-    var passo = Number(document.querySelector('#passo').value)
+    var inicio = document.querySelector('#inicio')
+    var fim = document.querySelector('#fim')
+    var passo = document.querySelector('#passo')
     var resultado = document.querySelector('#resultado')
 
-    ///contador
-    var cont = inicio
+    if(inicio.value.length == 0 || fim.value.length == 0 ){
+        alert('verifique os valores digitados!')
+    }else{
+        resultado.innerHTML = 'Contando: '
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
 
-    while(cont <= fim){
-        resultado.innerHTML =
-        cont++
+        if(p <= 0){
+            alert('considerando passo 1')
+            p = 1
+        }
+
+        if(i > f){
+            for(let c = i;c >= f;c -= p){
+                resultado.innerHTML += `${c} \u{1F449}`
+            }
+        }else{
+            for(let c = i;c <= f;c += p){
+                resultado.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        resultado.innerHTML += `\u{1F6A9}`
     }
-
 }
