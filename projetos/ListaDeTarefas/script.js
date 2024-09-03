9bdc35d8f7bc783855a1010d40e38905828bae03
davilucas <91565261+Davilucca22@users.noreply.github.com>
2024-09-02 23:00:33 -0300

    tomatoes

--- projetos/ListaDeTarefas/script.js
+++ projetos/ListaDeTarefas/script.js
@@ -1,8 +1,6 @@
 let tarefa = document.querySelector('#tarefa')
 let lista = document.querySelector('#Lista')
 
-const BotaoaddTarefa = document.querySelector('#botao')
-
 function limpaTarefa(){
     tarefa.value = ''
     tarefa.focus()
@@ -12,27 +10,23 @@ function addtarefa(tarefa){
     if (!tarefa.value){
         alert('Digite alguma tarefa para adicionar a Lista')
     }else{
-        let item = document.createElement('li')
         const apaga = document.createElement('button')
-        apaga.innerText = 'Excluir' 
+       let item = document.createElement('li')
+        apaga.innerText = 'X' 
         item.innerHTML = tarefa.value
-        item.appendChild(apaga)
         lista.appendChild(item)
+        item.appendChild(apaga)
         limpaTarefa()
 
         apaga.addEventListener('click',function(){
             item.remove() 
-        })
+        }) 
     }
 }
 
-
+//captura o enter
 tarefa.addEventListener('keypress',function(e){
     if(e.keyCode === 13){
         addtarefa(tarefa)
     }
-})
-
-BotaoaddTarefa.addEventListener('click',function(){
-    addtarefa(tarefa)
 })
