a315b258d19d71c315bce62124b2b387846ee50d
davilucas <91565261+Davilucca22@users.noreply.github.com>
2024-09-04 23:01:18 -0300

    lojinha do Ze

--- projetos/loja_virtual/script.js
+++ projetos/loja_virtual/script.js
@@ -17,3 +17,42 @@ function links(){
 }
 
 links()
+
+//objeto base para criar o produto
+function Produtos(nome,preço,img){
+    this.nome = nome,
+    this.preço = preço,
+    this.img = img
+}
+
+
+//funçao que cria uma div e uma imagem do produto,joga a imagem dentro da div e a div dentro da section no HTML
+Produtos.prototype.mostraProduto = function () {
+    const fundo = document.querySelector('#produtos')
+    const div = document.createElement('div')
+    const img = document.createElement('img')
+    img.setAttribute('src',this.img)
+    div.appendChild(img)
+    fundo.appendChild(div)
+
+}
+
+//produto controle
+const Controle = new Produtos('controle',150,'imagens/controle.png')
+//associa o prototype do controle ao do Produtos
+Controle.prototype = Object.create(Produtos.prototype)
+Controle.prototype.constructor = Controle
+
+//chama a funçao mostraProduto
+Controle.mostraProduto()
+console.log(Controle)
+
+//produto monitor
+const Monitor = new Produtos('monitor',200,'imagens/monitor.png')
+
+Monitor.prototype = Object.create(Produtos.prototype)
+Monitor.prototype.constructor = Monitor
+
+Monitor.mostraProduto()
+console.log(Monitor)
+
